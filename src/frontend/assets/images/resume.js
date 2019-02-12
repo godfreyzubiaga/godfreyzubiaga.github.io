1ba528778a8ddc06bb0219f1fbf8377c7ff8ea66
Godfrey Zubiaga <godfreyzubiaga@gmail.com>
2019-02-12 22:25:00 +0800

    add resume link

---
+++ src/frontend/assets/images/resume.js
@@ -0,0 +1,1 @@
+export default 'https://res.cloudinary.com/starksten/image/upload/v1549981338/document.png';
--- src/frontend/components/Footer/index.jsx
+++ src/frontend/components/Footer/index.jsx
@@ -5,77 +5,81 @@ import bottomMiddle from '../../assets/images/bottomMiddle';
 import github from '../../assets/images/github';
 import twitter from '../../assets/images/twitter';
 import email from '../../assets/images/email';
-import phone from '../../assets/images/phone';
+import resume from '../../assets/images/resume';
 import Item from './Item';
 
 const StyledDiv = styled.div`
-  background: ${gray};
-  color: white;
-  height: auto;
-  padding: 20px 0;
+	background: ${gray};
+	color: white;
+	height: auto;
+	padding: 20px 0;
 `;
 
 const TopBG = styled.div`
-  position: relative;
-  top: -30px;
-  background: url(${bottomMiddle});
-  background-size: 100% 100%;
-  width: 100%;
-  height: 200px;
+	position: relative;
+	top: -30px;
+	background: url(${bottomMiddle});
+	background-size: 100% 100%;
+	width: 100%;
+	height: 200px;
 
-  @media (max-width: 1000px) {
-    display: none;
-  }
+	@media (max-width: 1000px) {
+		display: none;
+	}
 `;
 
 const Container = styled.div`
-  padding: 0 20px;
-  display: flex;
-  @media screen and (min-width: 1000px) {
-    width: 100%;
-    margin: 0 auto;
-    display: flex;
-    justify-content: space-evenly;
-    flex-wrap: wrap;
-    padding: 0;
-  }
+	padding: 0 20px;
+	display: flex;
+	@media screen and (min-width: 1000px) {
+		width: 100%;
+		margin: 0 auto;
+		display: flex;
+		justify-content: space-evenly;
+		flex-wrap: wrap;
+		padding: 0;
+	}
 
-  @media (max-width: 1000px) {
-    flex-direction: column;
-    align-items: center;
-  }
+	@media (max-width: 1000px) {
+		flex-direction: column;
+		align-items: center;
+	}
 `;
 
 const ItemContainer = styled.div`
-  @media screen and (min-width: 1000px) {
-    margin: 0 auto;
-  }
+	@media screen and (min-width: 1000px) {
+		margin: 0 auto;
+	}
 `;
 
 export default () => (
-  <StyledDiv>
-    <TopBG />
-    <Container>
-      <ItemContainer>
-        <Item
-          data="Github Link"
-          link="https://github.com/godfreyzubiaga"
-          image={github}
-        />
-      </ItemContainer>
-      <ItemContainer>
-        <Item
-          data="Twitter Link"
-          link="https://mobile.twitter.com/freymwork"
-          image={twitter}
-        />
-      </ItemContainer>
-      <ItemContainer>
-        <Item data="(+63) 9454602013" image={phone} />
-      </ItemContainer>
-      <ItemContainer>
-        <Item data="godfreyzubiaga@gmail.com" image={email} />
-      </ItemContainer>
-    </Container>
-  </StyledDiv>
+	<StyledDiv>
+		<TopBG />
+		<Container>
+			<ItemContainer>
+				<Item
+					data="Github Link"
+					link="https://github.com/godfreyzubiaga"
+					image={github}
+				/>
+			</ItemContainer>
+			<ItemContainer>
+				<Item
+					data="Twitter Link"
+					link="https://mobile.twitter.com/freymwork"
+					image={twitter}
+				/>
+			</ItemContainer>
+			<ItemContainer>
+				<Item data="godfreyzubiaga@gmail.com" image={email} />
+			</ItemContainer>
+			<ItemContainer>
+				<Item
+					data="Resume"
+					link="https://drive.google.com/file/d/1gRMrWYiH5NyumQZyvgTAN5aa996sVPNt/view?usp=sharing"
+					image={resume}
+				/>
+			</ItemContainer>
+		</Container>
+	</StyledDiv>
 );
